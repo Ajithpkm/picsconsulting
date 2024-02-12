002b9a7cfa9f00df1b81596dae69c92ddfb8dc4c
Mohanavel <mohan.velu89@gmail.com>
2024-02-12 17:37:38 +0530

    tabs changes

--- src/component/Dashboard.js
+++ src/component/Dashboard.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import { Grow, Card, CardContent, Box, Grid, Radio, RadioGroup, Chip, FormControlLabel, FormControl, ListItemText, ListItemIcon, List, ListItem, ListItemAvatar, Typography } from '@mui/material';
@@ -33,7 +33,40 @@ const style = {
     border: '2px solid #000',
     boxShadow: 24,
     p: 4,
-  };
+};
+interface TabPanelProps {
+    children?: React.ReactNode;
+    index: number;
+    value: number;
+}
+
+function TabPanel(props: TabPanelProps) {
+    const { children, value, index, ...other } = props;
+
+    return (
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`vertical-tabpanel-${index}`}
+            aria-labelledby={`vertical-tab-${index}`}
+            {...other}
+        >
+            {value === index && (
+                <Box sx={{ p: 3 }}>
+                    <Typography>{children}</Typography>
+                </Box>
+            )}
+        </div>
+    );
+}
+
+function a11yProps(index: number) {
+    return {
+        id: `vertical-tab-${index}`,
+        'aria-controls': `vertical-tabpanel-${index}`,
+    };
+}
+
 const Dashboard = () => {
     const [value, setValue] = React.useState(0);
     const [open, setOpen] = React.useState(false);
@@ -50,7 +83,7 @@ const Dashboard = () => {
         }
         window.addEventListener('scroll', handleScroll)
         return () => window.removeEventListener('scroll', handleScroll);
-    },[])
+    }, [])
 
     return (
         <div>
@@ -255,18 +288,29 @@ const Dashboard = () => {
                         </section>
 
                         <section className='small-section'>
-                        <Button onClick={handleOpen}>Open modal</Button>
+                            <Button onClick={handleOpen}>Open modal</Button>
                             <div className='container'>
                                 <Tabs
+                                    orientation="horizontal"
+                                    variant="scrollable"
                                     value={value}
                                     onChange={handleChange}
-                                    aria-label="icon position tabs example"
+                                    aria-label="horizontal tabs example"
+                                    sx={{ borderRight: 1, borderColor: 'divider' }}
                                 >
-                                    <Tab icon={<PhoneIcon />} iconPosition="end" label="one" />
-                                    <Tab icon={<PhoneIcon />} iconPosition="end" label="two" />
-                                    <Tab icon={<PhoneIcon />} iconPosition="end" label="three" />
-                                    
+                                    <Tab label="Item One" {...a11yProps(0)} />
+                                    <Tab label="Item Two" {...a11yProps(1)} />
+                                    <Tab label="Item Three" {...a11yProps(2)} />
                                 </Tabs>
+                                <TabPanel value={value} index={0}>
+                                    Item One
+                                </TabPanel>
+                                <TabPanel value={value} index={1}>
+                                    Item Two
+                                </TabPanel>
+                                <TabPanel value={value} index={2}>
+                                    Item Three
+                                </TabPanel>
                             </div>
                         </section>
                     </main>
