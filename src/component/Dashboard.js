35dd8913898241f1fbaa6d08663bfb65f846e27d
Mohanavel <mohan.velu89@gmail.com>
2024-04-09 19:31:18 +0530

    latest

--- src/component/Dashboard.js
+++ src/component/Dashboard.js
@@ -238,8 +238,6 @@ const Dashboard = () => {
                                 <Typography className='modal-bx-body p-0' id="modal-modal-description" sx={{ mt: 2 }}>
                                     <div onClick={handleUsaClose} className='closemodal'><CloseIcon /></div>
                                     <iframe width="100%" height="315" src="https://www.youtube.com/embed/PpmD5vUlpCQ?si=u6WHnsqUHv1lnJN_" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-                                    
-                                    
                                 </Typography>
                             </Box>
                         </Modal>
